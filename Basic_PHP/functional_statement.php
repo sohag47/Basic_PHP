87ea2af4174bf19f5f5972849865db2a02780551
sohag47 <minhazul15-1630@diu.edu.bd>
2023-02-09 00:37:23 +0600

    function done

--- Basic_PHP/functional_statement.php
+++ Basic_PHP/functional_statement.php
@@ -1,21 +1,55 @@
 <?php
+
+declare(strict_types=1);
 /*
 ! php function like javascript
 */
 
 //? basic function
-function testShow($message){
-    echo($message);
-} 
-testShow("Hello, it is me(sohag)"."\n");
-testShow("Hello, it is me(sohag)"."\n");
+function userInfo(string $name = '', string $email = '', int &$age = 0): array
+{
+    return $data = [
+        'name' => $name,
+        'email' => $email,
+        'age' => $age
+    ];
+}
+
+$age = 26;
+$result = userInfo("Minhazul Islam Sohag", "sohag@email.com", $age);
+var_dump($result);
+echo "\n";
+
 
 //? multiple value 
-function testSum(...$num){
+function testSum(...$num)
+{
     return var_dump($num);
 }
-echo testSum(1, 2,3,4,5,6);
+echo testSum(1, 2, 3, 4, 5, 6);
+
+//? Anonymous function 
+$greet = function ($name) {
+    echo $name;
+};
+$greet("Sohag \n");
+
+//? Inherit $message
+$message = "world";
+$example = function ($arg) use ($message) {
+    echo $arg . '' . $message . "\n";
+};
+
+$example("Hello");
+
 
 //? arrow function
+$y = 1;
+$fn1 = fn ($x) => $x + $y;
+echo $fn1(2);
+"\n";
 
-?>
+$fn2 = function ($x) use ($y) {
+    return $x + $y;
+};
+var_export($fn2(2));
