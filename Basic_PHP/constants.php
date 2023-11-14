656699ea39b0c1d440a072b7e19dc681ce1f6371
Minhazul Islam Sohag <minhazul.islamcse@gmail.com>
2023-11-14 21:56:56 +0600

    php constant done

---
+++ Basic_PHP/constants.php
@@ -0,0 +1,19 @@
+<?php
+//! PHP Constants
+/*
+? 1. define()
+? 2. const
+*/
+
+
+//? 1. Define()
+define("PI", 3.1416);
+echo "PI value => ".PI."\n";
+
+
+//? 2. const
+const TAX = 10;
+echo "TAX value => ".TAX."\n";
+
+
+?>
