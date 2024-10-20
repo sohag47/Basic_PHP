212ab0960bea8480396b270ca3d3245a8365a9c3
sohag47 <minhazul.islamcse@gmail.com>
2024-10-20 07:43:33 +0600

    index array

--- array/Indexed_array.php
+++ array/Indexed_array.php
@@ -3,23 +3,27 @@
 ! array
 ! 3 types of array
 ? 1. Indexed array
-? 2. Association array 
+? 2. Association array
 ? 3. Multidimensional arrays
 */
 
+//!  01. Indexed array:
+$cars = ["volvo", "BMW", "Toyota"];
+var_dump($cars);
 
-//!  01. Indexed array: 
-$cars = array('volvo', 'BMW', 'Toyota');
 
-//? Read data from array: 
-function readArray($cars){
-    foreach($cars as $index=>$car){
-        echo($index." ".$car."<br/>");
+[$car1, $car2, $car3] = $cars;
+echo("car1: {$car1}, car2: {$car2}, car3: {$car3} \n");
+
+//? Read data from array:
+function readArray($cars)
+{
+    foreach ($cars as $index => $car) {
+        echo "index: {$index}, car: {$car} \n";
     }
-    echo("Total number of element: ".count($cars)."<br/>");
+    echo "Total number of element: " . count($cars) .  "\n";
 }
+
+array_push($cars, "Ford");
 readArray($cars);
-
-
-
 ?>
