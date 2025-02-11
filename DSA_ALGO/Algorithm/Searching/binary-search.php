f6a988ee1e2ad881c30b6715413f0ed36561ee2e
sohag47 <minhazul.islamcse@gmail.com>
2025-02-11 12:04:25 +0600

    Refactor binary search implementation to handle empty arrays and improve search logic

--- DSA_ALGO/Algorithm/Searching/binary-search.php
+++ DSA_ALGO/Algorithm/Searching/binary-search.php
@@ -3,33 +3,36 @@
 //! Binary search in PHP
 
 function binarySearch (array $data, int $search_item): int {
-    $left = 0;
-    $right = count($data) - 1;
+    if (count($data) === 0) return false; 
+    $low = 0;
+    $high = count($data) - 1;
 
 
-    while ($left <= $right) {
-        $mid = ($left + $right) / 2; // 5
-        return $mid;
-        // // If the search item is equal to the middle element it is found
-        // if($data[$mid] == $search_item){
-        //     return $mid;
+    while ($low <= $high) {
 
-        // }
-        // if($data[$mid] < $search_item){
-        //     // search item must be in the right half array
-        //     $data = array_slice($data, 0, $mid);
-        // }else {
-        //     // search item must be in the left half array
-        //     $data = array_slice($data, $mid + 1);
-        // }
+        // get the middle element
+        $mid =  floor(($low + $high) / 2);
+
+        // If the search item is equal to the middle element it is found
+        if($data[$mid] == $search_item){
+            return $mid;
+        }
         
-        
+        if ($search_item < $data[$mid]) { 
+            // search the left side of the array 
+            $high = $mid -1; 
+        } 
+        else { 
+            // search the right side of the array 
+            $low = $mid + 1; 
+        }
     }
-    return -1;
+    // If we reach here element x doesn't exist 
+    return false;
 }
 
 
 $data = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 $search_item = 4;
 $output = binarySearch($data, $search_item);
-echo $output;
+echo $output === false ? "Element is not present in array" : "Element is present at index " . $output.PHP_EOL;
